39ff430aee9fa4668b229eb176cad6cc91d545a9
Dan Bikle <dan@bot4.us>
2011-11-02 17:33:39 -0700

    hn17

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hn17::Application.config.secret_token = '8d829c3edc3b98d0a4564b21e758188563f89a82afaccd39d72e38fd2498f36ea7e6275e527005c6d88d7057635d151d3f66cda92e44a546c6336ea455547c14'
