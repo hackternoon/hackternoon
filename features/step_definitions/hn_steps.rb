bd6cf79acedeb5f4031c5206158eacbac302f6b3
Dan Bikle <bikle@bikle.com>
2011-11-27 11:08:30 +0000

    users/confirmation.feature

--- features/step_definitions/hn_steps.rb
+++ features/step_definitions/hn_steps.rb
@@ -5,6 +5,13 @@ And /^debug$/ do
   p 'debugger called'
 end
 
+And /^I_confirm$/ do
+  @usr = User.find_by_email 'user@test.com'
+  @token = @usr.confirmation_token
+  @cpath = "/users/confirmation?confirmation_token=#{@token}"
+  visit @cpath
+end
+
 And /^saop$/ do
   save_and_open_page
 end
