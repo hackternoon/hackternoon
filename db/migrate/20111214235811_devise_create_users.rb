8bab5e331ae6218a31186f61a90520913bc9fb79
Dan Bikle <bikle@bikle.com>
2011-12-14 16:07:18 -0800

    vi_20111214235811_devise_create_users.rb

--- db/migrate/20111214235811_devise_create_users.rb
+++ db/migrate/20111214235811_devise_create_users.rb
@@ -1,10 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
-  def change
+  def self.up
     create_table(:users) do |t|
       t.database_authenticatable :null => false
       t.recoverable
       t.rememberable
       t.trackable
+      t.confirmable
 
       # t.encryptable
       # t.confirmable
@@ -17,9 +18,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
   end
 
+  def self.down
+    drop_table :users
+  end
 end
