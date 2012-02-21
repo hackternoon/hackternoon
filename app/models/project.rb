6062d002d093a3e4ceb07d08fa74378fe4e79e6d
Dan Bikle <dan@bot4.us>
2012-02-21 07:36:03 +0000

    prep4rails3.2upgrade

--- app/models/project.rb
+++
@@ -1,7 +0,0 @@
-class Project < ActiveRecord::Base
-  belongs_to :user
-  def save
-    # I want each project to have an owner
-    super if self.user_id.present?
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hn21::Application.config.secret_token = '1a45ffffd592bc2063a2971644247fefafb95fde5589313533c4603a731f77bbb5b75f8283757a009b31b861a754912a77597061a49d3326ec67a0a8ca02fef5'
