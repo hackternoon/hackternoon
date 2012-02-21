db2a01e276a3f58cec208baa150c7593c5482ac6
Dan Bikle <dan@bot4.us>
2012-02-21 08:01:08 +0000

    rails3.2upgrade_started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hn24::Application.config.secret_token = 'd97d17451afd16247d4e7fefd87a3a51f2e27c936533dba28000ac6019034dea48d7db491bf51bacfdbc14898b717462550c0357d02303bdbe6324d6b80ba04d'
