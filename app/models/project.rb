4d193ee82ef48d8fef88162592c482e2c34e081d
Dan Bikle <dan@bot4.us>
2012-02-23 09:47:14 +0000

    Project_belongs_to_:user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
+  def save
+    # I want each project to have an owner
+    super if self.user_id.present?
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  has_many :projects
+
   validates_uniqueness_of :email, :case_sensitive => false
 
   # Setup accessible (or protected) attributes for your model
