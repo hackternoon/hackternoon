291c3771108f80c3d31e3da0124bc199b4eb1f32
Dan Bikle <dan@bot4.us>
2012-02-29 09:13:33 +0000

    master_now_empty

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hn24::Application.config.secret_token = '0e29ca7ac8879982de3443f499b7ecf6ccbe5309dce1c52078f61a99b4e90972f117b911e0751b65a93a3557e6d56ab5badc702ec22b2d32e586f280625d481a'
