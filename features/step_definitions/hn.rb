99af88f7bc6d2c92d6acee1e32bcc697afd69704
Dan Bikle <dan@bot4.us>
2012-03-01 23:18:11 +0000

    features/invitations/

--- features/step_definitions/hn.rb
+++ features/step_definitions/hn.rb
@@ -14,3 +14,24 @@ Then /^I should see page_has_selector "([^"]*)"$/ do |sel_name|
   page.should have_selector(sel_name)
 end
 
+Then /^I should see page_has_field "([^"]*)"$/ do |id|
+  page.should have_selector('input#' + id)
+end
+
+Then /^I login_as "(.*)\/(.*)"$/ do |email, password|
+  step %{I sign out}
+  step %{I go to the home page}
+  step %{I follow "Register"}
+  step %{I fill in "Email" with "#{email}"}
+  step %{I press "Signup"}
+  # get the conf-token from AR
+  @usr = User.find_by_email email
+  @token = @usr.confirmation_token
+  # visit conf page
+  @cpath = "/users/confirmation?confirmation_token=#{@token}"
+  visit @cpath
+  # enter the password
+  step %{I fill in "Password" with "#{password}"}
+  step %{I fill in "Password confirmation" with "#{password}"}
+  click_button "Press This"
+end
