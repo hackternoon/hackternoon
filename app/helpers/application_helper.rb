58411386f774e1e27e30311474d39215f342806e
Dan Bikle <dan@bot4.us>
2012-03-04 16:02:50 -0800

    i_own?(object)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def i_own?(object)
+    if(current_user.present? and object.user.present? and object.user.id.present? and (current_user.id == object.user.id))
+      true
+    else
+      false
+    end
+  end
+
 end
