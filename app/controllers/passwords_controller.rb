a4711eb5ec772133780875f8d4bdfbb6d7fe3a54
Dan Bikle <dan@bot4.us>
2012-03-16 15:02:10 +0000

    enhance controllers/passwords_controller.rb

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,6 @@
 class PasswordsController < Devise::PasswordsController
 
+  # Control flows here after user submits e-mail address to form for password reset:
   def create
     self.resource = resource_class.send_reset_password_instructions(params[resource_name])
     set_flash_message :notice, :send_instructions
